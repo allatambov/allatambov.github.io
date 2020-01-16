e1be27dd7ac8e6bd087a7d3448a31a866c62f30a
allatambov <allatambov@gmail.com>
2020-01-17 02:30:04 +0300

    add hh

---
+++ psms/script.js
@@ -0,0 +1,9 @@
+function unhover(element) {
+  element.setAttribute('src', 'hh1.png');
+  element.setAttribute('width', '50%');
+}
+
+function hover(element) {
+  element.setAttribute('src', 'hh2.png');
+  element.setAttribute('width', '50%');
+}
