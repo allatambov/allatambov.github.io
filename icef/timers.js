fe579210027acb7e1a95797414665353ee552144
allatambov <allatambov@gmail.com>
2020-03-21 15:58:50 +0300

    add 3-4

--- icef/timers.js
+++ icef/timers.js
@@ -3,6 +3,7 @@ countdown('timer1', "Mar 11, 2020 15:00:00");
 countdown('timer2', "Mar 14, 2020 23:59:59");
 countdown('timer3', "Mar 23, 2020 18:00:00");
 countdown('timer4', "Mar 26, 2020 18:00:00");
+countdown('timer5', "Mar 30, 2020 23:59:59");
 
 function countdown(element, point){
 
