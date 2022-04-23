7a2f0df907645f1d263e158c920e6c6847eae1dd
Alla Tambovtseva <allatambov@gmail.com>
2022-04-24 01:17:12 +0300

    Update problem06.py

--- PyProgPerm/practice/practice02/problem06.py
+++ PyProgPerm/practice/practice02/problem06.py
@@ -7,27 +7,30 @@ else:
     print("Не молодец")
 
 # b - hard
+
 x = input()
+
 if x == "0":
     print("Не молодец!")
-elif x.startswith("-") == False:
-    if x.isdigit():
-        print("Молодец!")
-    elif x.count(".") == 1:
-        if x.replace(".", "").isdigit():
+else:
+    if x.startswith("-") == False:
+        if x.coun-t(".") == 1:
+            if x.replace(".", "").isdigit():
+                print("Молодец!")
+            else:
+                print("Это не число!")
+        elif x.isdigit():
             print("Молодец!")
         else:
-            print("Не число!")
+            print("Это не число!")
     else:
-        print("Не число!")
-    elif x[1:].isdigit():
-            print("Не молодец!")
-        elif x.count(".") == 1:
+        if x.count(".") == 1:
             if x[1:].replace(".", "").isdigit():
                 print("Не молодец!")
             else:
-                print("Не число!")
+                print("Это не число!")
+        elif x[1:].isdigit():
+            print("Не молодец!")
         else:
-            print("Не число!")
-
-
+            print("Это не число!")
+            
